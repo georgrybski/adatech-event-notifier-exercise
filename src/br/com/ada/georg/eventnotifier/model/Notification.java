49cb2a804155cda98f0887ca01d44529e77077f7
georgrybski <rybskigeorg@gmail.com>
2023-01-30 17:46:35 -0300

    Added Notification class

--- src/br/com/ada/georg/eventnotifier/model/Notification.java
+++ src/br/com/ada/georg/eventnotifier/model/Notification.java
@@ -1,2 +1,25 @@
-package br.com.ada.georg.eventnotifier.model;public class Notification {
+package br.com.ada.georg.eventnotifier.model;
+
+import java.time.LocalDateTime;
+
+public class Notification {
+    private String name;
+    private String message;
+
+    private String dateTime;
+
+    public Notification(String name, String message, String dateTime) {
+        this.name = name;
+        this.message = message;
+        this.dateTime = dateTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " recieved a message: -> " + message + " - " + dateTime;
+    }
 }
