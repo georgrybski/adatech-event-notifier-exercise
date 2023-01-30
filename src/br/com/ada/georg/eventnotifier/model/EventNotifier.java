3321c84eb489a09f570fb0997060d5b42a203e4d
georgrybski <rybskigeorg@gmail.com>
2023-01-30 17:47:50 -0300

    Added EventNotifier class

--- src/br/com/ada/georg/eventnotifier/model/EventNotifier.java
+++ src/br/com/ada/georg/eventnotifier/model/EventNotifier.java
@@ -1,4 +1,27 @@
-package br.com.ada.georg.eventnotifier.control;
+package br.com.ada.georg.eventnotifier.model;
+
+import br.com.ada.georg.eventnotifier.InputHandlerImp;
+import br.com.ada.georg.eventnotifier.repository.InMemoryDatabase;
+import br.com.ada.georg.eventnotifier.view.Printer;
 
 public class EventNotifier {
+    private InputHandlerImp input = new InputHandlerImp();
+    private Printer printer = new Printer();
+    private InMemoryDatabase database = new InMemoryDatabase();
+
+    public void showAllNotifications() {
+        printer.printAllNotifications(database.getNamesToNotifications());
+    }
+
+    public void addNotification() {
+        database.addNotification(input.getNotification());
+    }
+
+    public void wipeNotifications() {
+        database.wipeNotifications();
+    }
+
+    public Integer getNumberOfMessages() {
+        return input.getNumberOfMessages();
+    }
 }
